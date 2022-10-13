8c38cc49c12cfb53a6a14817ff97a7cc20a64a26
OmerYs <omeryshai@gmail.com>
2022-10-13 15:22:21 +0300

    s

---
+++ app/src/main/java/com/example/calendar_oy/TableCalendar.java
@@ -0,0 +1,14 @@
+package com.example.calendar_oy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TableCalendar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table_calendar);
+    }
+}
