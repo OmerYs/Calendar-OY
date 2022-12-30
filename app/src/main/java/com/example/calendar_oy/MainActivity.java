31c16e9ff5f858851968362a43bc5745c08c8956
OmerYs <omeryshai@gmail.com>
2022-12-30 19:16:52 +0200

    sharedprefrences

--- app/src/main/java/com/example/calendar_oy/MainActivity.java
+++ app/src/main/java/com/example/calendar_oy/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.calendar_oy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private CardView BucketCard, ListCard, CalendarCard, TimelineCard, NotesCard, SettingCard;
+    public SharedPref sharedPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        useSharedPreferences();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BucketCard = (CardView) findViewById(R.id.bucketCard);
+        BucketCard.setOnClickListener((View.OnClickListener)this);
+        ListCard = (CardView) findViewById(R.id.listCard);
+        ListCard.setOnClickListener((View.OnClickListener)this);
+        CalendarCard = (CardView) findViewById(R.id.calendarCard);
+        CalendarCard.setOnClickListener((View.OnClickListener)this);
+        TimelineCard = (CardView) findViewById(R.id.timelineCard);
+        TimelineCard.setOnClickListener((View.OnClickListener)this);
+        NotesCard = (CardView) findViewById(R.id.notesCard);
+        NotesCard.setOnClickListener((View.OnClickListener)this);
+        SettingCard = (CardView) findViewById(R.id.settingsCard);
+        SettingCard.setOnClickListener((View.OnClickListener)this);
+    }
+
+    @Override
+    public void onClick(View view){
+        Intent i;
+        switch (view.getId()){
+            case R.id.bucketCard:;
+            case R.id.listCard:;
+            case R.id.calendarCard:;
+            case R.id.timelineCard:;
+            case R.id.notesCard:;
+            case R.id.settingsCard: i = new Intent(this, SettingsPage.class);finish();startActivity(i);break;
+        }
+    }
+
+    public void useSharedPreferences(){
+        sharedPref = new SharedPref(this);
+        if (sharedPref.loadNightModeState()==true){
+            setTheme(R.style.Theme_CalendarOYDark);
+        }
+        else{
+            setTheme(R.style.Theme_CalendarOY);
+        }
     }
 }
---
+++ app/src/main/java/com/example/calendar_oy/SettingsPage.java
@@ -0,0 +1,59 @@
+package com.example.calendar_oy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import java.util.Set;
+
+public class SettingsPage extends AppCompatActivity {
+    private Switch darkModeSwitch;
+    public SharedPref sharedPref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        sharedPref = new SharedPref(this);
+        if (sharedPref.loadNightModeState()==true){
+            setTheme(R.style.Theme_CalendarOYDark);
+        }
+        else{
+            setTheme(R.style.Theme_CalendarOY);
+        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_page);
+
+        darkModeSwitch = findViewById(R.id.darkMode_switch);
+
+        if(sharedPref.loadNightModeState() == true){
+            darkModeSwitch.setChecked(true);
+        }
+        darkModeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked){
+                    sharedPref.setNightModeState(true);
+                }
+                else{
+                    sharedPref.setNightModeState(false);
+                }
+                restartApp();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent i = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+     public void restartApp(){
+        Intent i = new Intent(getApplicationContext(),SettingsPage.class);
+        startActivity(i);
+        finish();
+    }
+}
