eb17a8d69dce6b6d9f9b5f50aab43b2ff2bdedae
OmerYs <omeryshai@gmail.com>
2023-03-21 00:21:30 +0200

    better color design and patterns, also implementation of themes into some pages

--- app/src/main/java/com/example/calendar_oy/SettingsPage.java
+++ app/src/main/java/com/example/calendar_oy/SettingsPage.java
@@ -4,41 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.CompoundButton;
-import android.widget.Switch;
-
-import java.util.Set;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class SettingsPage extends AppCompatActivity {
-    private Switch darkModeSwitch;
+    private LinearLayout lightModeLayout;
+    private LinearLayout darkModeLayout;
     public SharedPref sharedPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         sharedPref = new SharedPref(this);
-        if (sharedPref.loadNightModeState()==true){
+        if (sharedPref.loadNightModeState() == true) {
             setTheme(R.style.Theme_CalendarOYDark);
-        }
-        else{
+        } else {
             setTheme(R.style.Theme_CalendarOY);
         }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_page);
 
-        darkModeSwitch = findViewById(R.id.darkMode_switch);
+        lightModeLayout = findViewById(R.id.lightModeLayout);
+        darkModeLayout = findViewById(R.id.darkModeLayout);
 
-        if(sharedPref.loadNightModeState() == true){
-            darkModeSwitch.setChecked(true);
-        }
-        darkModeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        lightModeLayout.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                if(isChecked){
-                    sharedPref.setNightModeState(true);
-                }
-                else{
-                    sharedPref.setNightModeState(false);
-                }
+            public void onClick(View v) {
+                sharedPref.setNightModeState(false);
+                restartApp();
+            }
+        });
+
+        darkModeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sharedPref.setNightModeState(true);
                 restartApp();
             }
         });
@@ -46,13 +45,13 @@ public class SettingsPage extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        Intent i = new Intent(getApplicationContext(),MainActivity.class);
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(i);
         finish();
     }
 
-    public void restartApp(){
-        Intent i = new Intent(getApplicationContext(),SettingsPage.class);
+    public void restartApp() {
+        Intent i = new Intent(getApplicationContext(), SettingsPage.class);
         startActivity(i);
         finish();
     }
