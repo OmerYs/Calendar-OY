379cd7655d6754220431e0e1d16bfa07b46c6693
OmerYs <omeryshai@gmail.com>
2023-03-22 18:02:20 +0200

    finished signup and login with firebase authentication

--- app/src/main/java/com/example/calendar_oy/LoginActivity.java
+++ app/src/main/java/com/example/calendar_oy/LoginActivity.java
@@ -1,13 +1,81 @@
 package com.example.calendar_oy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private EditText loginEmail, loginPassword;
+    private Button loginButton;
+    private TextView signupRedirectText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+        loginEmail = findViewById(R.id.login_email);
+        loginPassword = findViewById(R.id.login_password);
+        loginButton = findViewById(R.id.login_button);
+        signupRedirectText = findViewById(R.id.signupRedirectText);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = loginEmail.getText().toString();
+                String password = loginPassword.getText().toString();
+
+                if (!user.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(user).matches()) {
+                    if (!password.isEmpty()) {
+                        auth.signInWithEmailAndPassword(user, password)
+                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        Toast.makeText(LoginActivity.this, "Login was successful", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                        finish();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                } else {
+                    loginPassword.setError("Password can't be empty");
+                }
+                } else if (user.isEmpty()) {
+                    loginEmail.setError("Email can't be empty");
+                } else {
+                    loginEmail.setError("Please enter a valid email");
+                }
+            }
+        });
+
+        signupRedirectText.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+                finish();
+            }
+        });
     }
 }
